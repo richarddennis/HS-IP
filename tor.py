165c2b8e305221ffd64e7e64766bef5af1354e25
Richard Dennis <mad_raptor@hotmail.com>
2015-03-13 11:57:16 +0000

    Update tor.py

--- tor.py
+++ tor.py
@@ -23,7 +23,6 @@ from time import sleep
 import threading
 import errno
 
-from torfuncs import *
 from rendFuncs import *
 
 import mysql.connector
@@ -33,10 +32,13 @@ import mysql.connector
 db=mysql.connector.connect(user='root', password ='edge#540', database='hsip')
 cursor=db.cursor()
 
-name = "silkroad6ownowfk"
+#Onion name
+onion_Add = "silkroad6ownowfk" #Hidden service address , can be with or without the .onion at the end
 
-cursor.execute("CREATE TABLE IF NOT EXISTS " + name + 
-	"(identityb32  CHAR(40) NOT NULL,"
+#Creates a new table for each onion address
+cursor.execute("CREATE TABLE IF NOT EXISTS " + onion_Add + 
+	"(consensus  CHAR(40) NOT NULL," 
+	+ "identityb32  CHAR(40) NOT NULL,"
 	+ "pubdate  CHAR(40) NOT NULL,"
 	+ "dirport  CHAR(6) NOT NULL,"
 	+ "ip  CHAR(40) NOT NULL,"
@@ -50,25 +52,13 @@ cursor.execute("CREATE TABLE IF NOT EXISTS " + name +
 	+ "pubtime  CHAR(40) NOT NULL)"
 	)
 
-
-# #Will soon remove, want to put all data in a db rather than txt files
-# extension =".txt"
-# x = 0
-# fileName = name + extension
-# while(os.path.exists(fileName)):
-#     x = x + 1
-#     fileName = name + str(x) + extension
-
-# file = open(fileName, 'w+')
-
-
 i = 0
 consensus.fetchConsensus(i) #Retrieves the consensus 
 
+print consensus.consensus_name
 
 print "Retriving hidden service descriptor" #Keeping the user informed
 #"kpvz7ki2v5agwt35"#Hidden Wiki   #"3g2upl4pq6kufc4m"#duck duck go     #"idnxcnkne4qt76tg" #homepage of the Tor project   #All various hidden service addresses, some dont work as temporarily gone down etc
-onion_Add = "silkroad6ownowfk" #Hidden service address , can be with or without the .onion at the end
 
 responsible_HSDir_list = [] #Setting up variables to take an array list to be used later on
 descriptor_id_list = [] #Setting up variables to take an array list to be used later on
@@ -79,11 +69,35 @@ for i in range(0, 2):
     responsible_HSDir = find_responsible_HSDir(descriptor_id)# Passes the descriptor to the find_responsible_HSDir function in rendFunccs (returns the responsible hidden service directories for the selected hidden service)
     responsible_HSDir_list.append(responsible_HSDir) # Saves all responsible HSDir information in a list to use later (3 responsible hidden service directories)
 
-ip_addresses, dirport, port, nickname, identity = extract_HSDir_data(responsible_HSDir_list) # Extracts the data from the reponsible Hidden service directories and assigns these to several variables for use later on
+identityb32, pubdate, orport, identityhash, nick, version, flags, identity, digest, pubtime = extract_HSDir_data(responsible_HSDir_list) # Extracts the data from the reponsible Hidden service directories and assigns these to several variables for use later on
 # web_addresses = connect_to_web_lookup(ip_addresses, dirport, descriptor_id_list) # Creates the IP address with Port numbers for all the responsible hidden service directories
 
-print responsible_HSDir_list
+print nick
+print len(nick) 
 
+l = 0
+
+while l < len(nick):
+	print nick[l]
+	l = l +1
+
+# Prepare SQL query to INSERT a record into the database.
+# sql = "INSERT INTO " + "`" + onion_Add + "`" + "(`nick`)"+ "VALUES" + nick[0]
+
+# Prepare SQL query to INSERT a record into the database.
+sql = """INSERT INTO """ + onion_Add +"""(nick)
+         VALUES ('+""" +nick[0]+"""')"""
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Commit your changes in the database
+   db.commit()
+except:
+   # Rollback in case there is any error
+   db.rollback()
+   print "error"
+# disconnect from server
+db.close()
 
 sys.exit(0)
 
