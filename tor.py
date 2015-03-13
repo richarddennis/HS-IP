a2019f8e3a8338dcb7593009e26da226e7da887d
Richard Dennis <mad_raptor@hotmail.com>
2015-03-13 15:12:06 +0000

    Update tor.py

--- tor.py
+++ tor.py
@@ -22,6 +22,7 @@ import string
 from time import sleep
 import threading
 import errno
+import re
 
 from rendFuncs import *
 
@@ -46,61 +47,73 @@ cursor.execute("CREATE TABLE IF NOT EXISTS " + onion_Add +
 	+ "identityhash  CHAR(40) NOT NULL,"
 	+ "nick  CHAR(40) NOT NULL,"
 	+ "version  CHAR(40) NOT NULL,"
-	+ "flags  CHAR(90) NOT NULL,"
-	+ "identity  CHAR(40) NOT NULL,"
+	+ "flags  VARCHAR(500) NOT NULL,"
+	+ "identity  VARCHAR(256) NOT NULL,"
 	+ "digest  CHAR(40) NOT NULL,"
 	+ "pubtime  CHAR(40) NOT NULL)"
 	)
 
-i = 0
-consensus.fetchConsensus(i) #Retrieves the consensus 
+h = 18 #Hour
+d = 21 #Day
+m = 03 #Month
+y = 2014 #Year
 
-print consensus.consensus_name
+while True:
 
-print "Retriving hidden service descriptor" #Keeping the user informed
-#"kpvz7ki2v5agwt35"#Hidden Wiki   #"3g2upl4pq6kufc4m"#duck duck go     #"idnxcnkne4qt76tg" #homepage of the Tor project   #All various hidden service addresses, some dont work as temporarily gone down etc
+	consensus_file_name = str(y) + "-" + str(m) + "-" + str(d) + "-" + str(h) + "-00-00-consensus"
+	print consensus_file_name 
+ 	consensus_name = "2014-03-21-18-00-00-consensus"
 
-responsible_HSDir_list = [] #Setting up variables to take an array list to be used later on
-descriptor_id_list = [] #Setting up variables to take an array list to be used later on
-
-for i in range(0, 2):
-    descriptor_id = get_descriptor_Id(onion_Add, i) #Passes the onion address and i to the get_descriptor_id function in rendFunccs ("descriptor-id" is a identifier that is calculated by the hidden service and its clients)
-    descriptor_id_list.append(descriptor_id) # Makes sure all 3 desciptor ids are stored
-    responsible_HSDir = find_responsible_HSDir(descriptor_id)# Passes the descriptor to the find_responsible_HSDir function in rendFunccs (returns the responsible hidden service directories for the selected hidden service)
-    responsible_HSDir_list.append(responsible_HSDir) # Saves all responsible HSDir information in a list to use later (3 responsible hidden service directories)
-
-identityb32, pubdate, orport, identityhash, nick, version, flags, identity, digest, pubtime = extract_HSDir_data(responsible_HSDir_list) # Extracts the data from the reponsible Hidden service directories and assigns these to several variables for use later on
-# web_addresses = connect_to_web_lookup(ip_addresses, dirport, descriptor_id_list) # Creates the IP address with Port numbers for all the responsible hidden service directories
-
-print nick
-print len(nick) 
-
-l = 0
-
-while l < len(nick):
-	print nick[l]
-	l = l +1
-
-# Prepare SQL query to INSERT a record into the database.
-# sql = "INSERT INTO " + "`" + onion_Add + "`" + "(`nick`)"+ "VALUES" + nick[0]
-
-# Prepare SQL query to INSERT a record into the database.
-sql = """INSERT INTO """ + onion_Add +"""(nick)
-         VALUES ('+""" +nick[0]+"""')"""
-try:
-   # Execute the SQL command
-   cursor.execute(sql)
-   # Commit your changes in the database
-   db.commit()
-except:
-   # Rollback in case there is any error
-   db.rollback()
-   print "error"
-# disconnect from server
-db.close()
-
-sys.exit(0)
+	consensus.fetchConsensus(consensus_name) #Retrieves the consensus 
 
 
+	print "Retriving hidden service descriptor" #Keeping the user informed
+	#"kpvz7ki2v5agwt35"#Hidden Wiki   #"3g2upl4pq6kufc4m"#duck duck go     #"idnxcnkne4qt76tg" #homepage of the Tor project   #All various hidden service addresses, some dont work as temporarily gone down etc
+
+	responsible_HSDir_list = [] #Setting up variables to take an array list to be used later on
+	descriptor_id_list = [] #Setting up variables to take an array list to be used later on
+
+	for i in range(0, 2):
+	    descriptor_id = get_descriptor_Id(onion_Add, i) #Passes the onion address and i to the get_descriptor_id function in rendFunccs ("descriptor-id" is a identifier that is calculated by the hidden service and its clients)
+	    descriptor_id_list.append(descriptor_id) # Makes sure all 3 desciptor ids are stored
+	    responsible_HSDir = find_responsible_HSDir(descriptor_id)# Passes the descriptor to the find_responsible_HSDir function in rendFunccs (returns the responsible hidden service directories for the selected hidden service)
+	    responsible_HSDir_list.append(responsible_HSDir) # Saves all responsible HSDir information in a list to use later (3 responsible hidden service directories)
+
+	identityb32, pubdate, dirport, ip, orport, identityhash, nick, version, flags, identity, digest, pubtime = extract_HSDir_data(responsible_HSDir_list) # Extracts the data from the reponsible Hidden service directories and assigns these to several variables for use later on
+	# web_addresses = connect_to_web_lookup(ip_addresses, dirport, descriptor_id_list) # Creates the IP address with Port numbers for all the responsible hidden service directories
+
+	# Prepare SQL query to INSERT a record into the database.
+	# sql = "INSERT INTO " + "`" + onion_Add + "`" + "(`nick`)"+ "VALUES" + nick[0]
+
+
+	c = 0
+
+	while c < len(nick):
+		format = """','"""
+
+		# print binascii.hexlify(identity[c])
+	# # #Not sure if i should do this ?
+	# binascii.hexlify(identity[c])
+
+		
+		# print flags[c]
+		flags[c] = str(flags[c]).replace("'", "")
+		identity[c] = str(identity[c]).replace("'", "")
+
+		sql = """INSERT INTO """ + onion_Add +"""(consensus, identityb32, pubdate, dirport, ip, orport, identityhash, nick, version, flags, identity, digest, pubtime)
+		         VALUES ('""" +consensus_name + format + identityb32[c] + format + pubdate[c] + format + dirport[c] + format + ip[c] + format + orport[c] + format + identityhash[c] + format + nick[c] + format + version[c] + format + flags[c] + format + binascii.hexlify(identity[c]) + format + digest[c] + format + pubtime[c] + """')"""
+
+		try:
+		   # Execute the SQL command
+		   cursor.execute(sql)
+		   # Commit your changes in the database
+		   db.commit()
+		except:
+		   # Rollback in case there is any error
+		   db.rollback()
+		   print "adding to db error"
+
+		c = c + 1
+		
 
 
