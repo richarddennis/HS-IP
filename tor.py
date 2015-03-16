58a7ae897753bc55fd8d8e7a4959b5c88906fe8c
Richard Dennis <mad_raptor@hotmail.com>
2015-03-16 19:32:34 +0000

    Update tor.py

--- tor.py
+++ tor.py
@@ -71,10 +71,6 @@ def calculate_and_write_hsdir(h,d,m,y):
 
 			consensus.fetchConsensus(consensus_file_name) #Retrieves the consensus 
 
-
-			# print "Retriving hidden service descriptor" #Keeping the user informed
-			#"kpvz7ki2v5agwt35"#Hidden Wiki   #"3g2upl4pq6kufc4m"#duck duck go     #"idnxcnkne4qt76tg" #homepage of the Tor project   #All various hidden service addresses, some dont work as temporarily gone down etc
-
 			responsible_HSDir_list = [] #Setting up variables to take an array list to be used later on
 			descriptor_id_list = [] #Setting up variables to take an array list to be used later on
 
@@ -137,61 +133,71 @@ def calculate_and_write_hsdir(h,d,m,y):
 
 # calculate_and_write_hsdir(h,d,m,y)
 def run_calculate(h,d,m,y):
-
-	if m == 01:
+	if m == 1:
 		while True:
 			d = calculate_and_write_hsdir(h,d,m,y)
 	   		if d == 31:
 	      			calculate_and_write_hsdir(h,d,m,y)
-	       			d = 01
+	       			d = 1
 	       			m = m + 1
-	if m == 02:
+	       			break
+	       			# if y == 2014:
+	       			# 	y = 2014
+	if m == 2:
 		while True:
 			d = calculate_and_write_hsdir(h,d,m,y)
 	   		if d == 28:
 	      			calculate_and_write_hsdir(h,d,m,y)
-	       			d = 01
+	       			d = 1
 	       			m = m + 1
-   	if m == 03:
+	       			break
+
+   	if m == 3:
    		while True:
-   			# print "In MARCH LOOP"
+   			print "In MARCH LOOP"
 	 		d = calculate_and_write_hsdir(h,d,m,y)
 	   		if d == 31:
         			calculate_and_write_hsdir(h,d,m,y)
-        			# print "March Done"
-        			d = 01
+        			print "March Done"
+        			d = 1
         			m = m + 1
-	if m == 04:
+        			break
+
+	if m == 4:
 		while True:
 			d = calculate_and_write_hsdir(h,d,m,y)
 	   		if d == 30:
 	      			calculate_and_write_hsdir(h,d,m,y)
-	       			d = 01
+	       			d = 1
 	       			m = m + 1
+	       			break
 
-	if m == 05:
+	if m == 5:
 		while True:
 			d = calculate_and_write_hsdir(h,d,m,y)
 	   		if d == 31:
 	      			calculate_and_write_hsdir(h,d,m,y)
-	       			d = 01
+	       			d = 1
 	       			m = m + 1
+	       			break
 
-	if m == 06:
+	if m == 6:
 		while True:
 			d = calculate_and_write_hsdir(h,d,m,y)
 	   		if d == 30:
 	      			calculate_and_write_hsdir(h,d,m,y)
-	       			d = 01
+	       			d = 1
 	       			m = m + 1
+	       			break
 
-	if m == 07:
+	if m == 7:
 		while True:
 			d = calculate_and_write_hsdir(h,d,m,y)
 	   		if d == 31:
 	      			calculate_and_write_hsdir(h,d,m,y)
-	       			d = 01
+	       			d = 1
 	       			m = m + 1
+	       			break
 
 	if m == 8:
 		while True:
@@ -200,6 +206,7 @@ def run_calculate(h,d,m,y):
 	      			calculate_and_write_hsdir(h,d,m,y)
 	       			d = 01
 	       			m = m + 1
+	       			break
 
 	if m == 9:
 		while True:
@@ -208,45 +215,39 @@ def run_calculate(h,d,m,y):
 	      			calculate_and_write_hsdir(h,d,m,y)
 	       			d = 01
 	       			m = m + 1
+	       			break
+
 	if m == 10:
 		while True:
 			d = calculate_and_write_hsdir(h,d,m,y)
 	   		if d == 31:
 	      			calculate_and_write_hsdir(h,d,m,y)
-	       			d = 01
+	       			d = 1
 	       			m = m + 1
+	       			break
+
 	if m == 11:
 		while True:
 			d = calculate_and_write_hsdir(h,d,m,y)
 	   		if d == 30:
 	      			calculate_and_write_hsdir(h,d,m,y)
-	       			d = 01
+	       			d = 1
 	       			m = m + 1
+	       			break
 
 	if m == 12:
 		while True:
 			d = calculate_and_write_hsdir(h,d,m,y)
 	   		if d == 31:
 	      			calculate_and_write_hsdir(h,d,m,y)
-	       			# y = y + 1	       			
-	       			# d = 01
-	       			# m = 01
 	       			break
-	       			
-# h = 00 #Hour
-# d = 28 #Day
-# m = 12 #Month
-# y = 2013 #Year
 
-# run_calculate(h,d,m,y)
 
 h = 00 #Hour
-d = 01 #Day
-m = 01 #Month
-y = 2014 #Year
+d = 07 #Day
+m = 10 #Month
+y = 2013 #Year
 
 run_calculate(h,d,m,y)
-
-
-# run_calculate(00,01,01,2014)
-# run_calculate(00,01,01,2015)
+run_calculate(00,1,1,2014)
+run_calculate(00,1,1,2015)
